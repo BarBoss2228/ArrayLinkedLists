5bcad75c21def6bba18d10d0fdc07fa1e29afd83
Anastasia <nastia.polpoudenko@gmail.com>
2021-11-09 02:36:07 +0300

    Started DoublyLinkedLists

---
+++ DoublyLinkedLists/DoublyNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedLists
+{
+    public class DoublyNode
+    {
+        public int Value { get; set; }
+        public DoublyNode Next { get; set; }
+        public DoublyNode Previous { get; set; }
+        public DoublyNode(int value)
+        {
+            Value = value;
+            Next = null;
+            Previus = null;
+        }
+    }
+}
